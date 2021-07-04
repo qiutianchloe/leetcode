9ec174f02fa93a3333c3253aa3b37f8c020cc7bd
Tian Qiu <tq@student.unimelb.edu.au>
2021-07-04 19:28:48 +0800

    76 Minimum Window Substring fixed

--- MinimumWIndowSubstring/minimumWindowSubstring.py
+++ MinimumWIndowSubstring/minimumWindowSubstring.py
@@ -1,55 +1,51 @@
 from collections import Counter
 class Solution(object):
     def minWindow(self, s, t):
-        if not t or not s: return ""
+        if t==s: return t
 
         #contruct the dictionary
-        charaDict = Counter(t)
+        d = dict(Counter(t))
         
         #define the left and right pointer, move the right point one each time, left as much as possible 
-        left = 0
-        right = 0
+        slow = 0
 
         #define the string that will be return, and start and end positin in the s. 
-        start = 0
-        end = 0
+        min_str = None
+        min_length = len(s)+1
 
         #the require number of character in the return value, and check whether so far we find enough number character
-        require_length = len(t)
         formed = 0
 
-        while right<len(s):
-            if s[right] in t:
-                charaDict[s[right]] = charaDict[s[right]]-1
-                if charaDict[s[right]]==0:
-                    formed = formed+1
+        for fast in range(len(s)):
+            ch = s[fast]
+            #skip if d[ch] doesn't matter, which means it is not in the string t 
+            fast+=1
+            if ch not in t:
+                continue
+
+            #use the ch to update d
+            d[ch]-=1
+            if d[ch]==0:
+                formed+=1
+            
+            #if all character are atisfied, then need to move the left pointer
+            while formed ==len(d) and slow <=fast:
+                #save the result
+                current_len = fast-slow
+                if current_len < min_length:
+                    min_length = current_len
+                    min_str = s[slow:fast]
+
+                #update the left boundary
+                ch = s[slow]
+                slow +=1
+                if ch not in t:
+                    continue
+                d[ch]+= 1
+                if d[ch]==1:
+                    formed -=1
                 
-                while formed == require_length and left <= right:
-                    # save the result 
-                    end = right 
-                    start = left
-                    if s[left] in t and charaDict[s[left]]+1==0:
-                        charaDict[s[left]] = charaDict[s[left]]+1
-                        if charaDict[s[left]]==1:
-                            formed = formed-1
-                    left = left + 1
-
-            right = right+1
-            print("--------------------------------------------------")
-            print("left and right is ")
-            print(left)
-            print(right)
-            print(charaDict)
-            print("formed equal: ")
-            print(formed)
-            print("start and end is ")
-            print(start)
-            print(end)
-            print("--------------------------------------------------")
-
-        if start==end and end == 0:
-            return ""
-        return s[start:end]
+        return min_str if min_str is not None else ""
 
 
 
